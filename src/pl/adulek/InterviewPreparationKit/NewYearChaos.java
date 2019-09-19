5100c361f5c644e2e57d1874d3e052a9f0b525dc
Adam Dunajewski <ad.dunajewski@gmail.com>
2019-09-19 08:54:53 +0200

    New Year Chaos Challenge done

--- src/pl/adulek/InterviewPreparationKit/NewYearChaos.java
+++ src/pl/adulek/InterviewPreparationKit/NewYearChaos.java
@@ -2,11 +2,31 @@ package pl.adulek.InterviewPreparationKit;
 
 import java.util.Scanner;
 
+/*
+2
+8
+5 1 2 3 7 8 6 4
+8
+1 2 5 3 7 8 6 4
+ */
+
 public class NewYearChaos {
 
     // Complete the minimumBribes function below.
-    static void minimumBribes(int[] q) {
-
+    public static void minimumBribes(int[] q) {
+        int counter = 0;
+        for (int i =0; i < q.length; i++) {
+            if (q[i] - (i+1) > 2) {
+                System.out.println("Too chaotic");
+                return;
+            }
+            for(int j = Math.max(0, q[i]-2); j < i; j++) {
+                if(q[j] > q[i]) {
+                    counter++;
+                }
+            }
+        }
+        System.out.println(counter);
 
     }
 
